f26fca8921ced66484e8c02f84b2b25d509b8fd9
Malcolm Bechard <malcolm@derivative.ca>
2021-05-03 15:53:12 -0400

    add licenses

--- src/OpenGLProgram.h
+++ src/OpenGLProgram.h
@@ -1,3 +1,18 @@
+/* Shared Use License: This file is owned by Derivative Inc. (Derivative) and
+ * can only be used, and/or modified for use, in conjunction with
+ * Derivative's TouchDesigner software, and only if you are a licensee who has
+ * accepted Derivative's TouchDesigner license or assignment agreement (which
+ * also govern the use of this file).  You may share a modified version of this
+ * file with another authorized licensee of Derivative's TouchDesigner software.
+ * Otherwise, no redistribution or sharing of this file, with or without
+ * modification, is permitted.
+ *
+ * TouchEngine
+ *
+ * Copyright © 2021 Derivative. All rights reserved.
+ *
+ */
+
 #pragma once
 
 #include "GL/glew.h"
